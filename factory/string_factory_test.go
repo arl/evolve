d2b2c45f43722ab9efc0ebab142fb4ec8544c97e
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-09-12 01:28:30 +0200

    Add error handling to StringFactory

--- factory/string_factory_test.go
+++ factory/string_factory_test.go
@@ -10,14 +10,35 @@ import (
 )
 
 func TestStringFactory(t *testing.T) {
+	const (
+		stringLength   = 8
+		populationSize = 10
+	)
 	rng := rand.New(rand.NewSource(99))
-	alphabet := "abcdefg"
-	stringLength := 8
-	populationSize := 10
 
-	cf := NewStringFactory(alphabet, stringLength)
-	pop := cf.GenerateInitialPopulation(populationSize, rng)
-	assert.Len(t, pop, populationSize)
+	t.Run("StringFactory with a ascii only aplhabet", func(*testing.T) {
+		sf, err := NewStringFactory("abcdefg", stringLength)
+		assert.Nil(t, err)
+		pop := sf.GenerateInitialPopulation(populationSize, rng)
+		assert.Len(t, pop, populationSize)
+	})
+
+	t.Run("StringFactory with non-ascii aplhabet", func(*testing.T) {
+		sf, err := NewStringFactory("日本語", stringLength)
+		assert.Nil(t, err)
+		pop := sf.GenerateInitialPopulation(populationSize, rng)
+		assert.Len(t, pop, populationSize)
+	})
+
+	t.Run("StringFactory with empty aplhabet", func(*testing.T) {
+		_, err := NewStringFactory("", stringLength)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("StringFactory with empty aplhabet", func(*testing.T) {
+		_, err := NewStringFactory("abcdefg", 0)
+		assert.NotNil(t, err)
+	})
 }
 
 func TestAsciiStringGenerator(t *testing.T) {
--- number/probability.go
+++ number/probability.go
@@ -5,40 +5,27 @@ import (
 	"math/rand"
 )
 
-/**
- * @param probability The probability value (a number in the range 0..1 inclusive).  A
- * value of zero means that an event is guaranteed not to happen.  A value of 1 means
- * it is guaranteed to occur.
- */
+// Probabilty represents a probability value (a number in the range 0..1 inclusive).
+//
+// A value of zero means that an event is guaranteed not to happen. A value of
+// 1 means it is guaranteed to occur.
 type Probability float64
 
 const (
-	/**
-	 * Convenient constant representing a probability of one.  An event with
-	 * a probability of one is a certainty.
-	 * @see #ZERO
-	 * @see #EVENS
-	 */
-
+	// ProbabilityOne is a convenient constant representing a probability of
+	// one, that is an event with a probability of one is a certainty.
 	ProbabilityOne Probability = 1
-	/**
-	 * Convenient constant representing a probability of 0.5 (used to model
-	 * an event that has a 50/50 chance of occurring).
-	 * @see #ZERO
-	 * @see #ONE
-	 */
 
+	// ProbabilityEven is a convenient constant representing a probability of
+	// 0.5 (used to model an event that has a 50/50 chance of occurring).
 	ProbabilityEven = 0.5
-	/**
-	 * Convenient constant representing a probability of zero.  If an event has
-	 * a probability of zero it will never happen (it is an impossibility).
-	 * @see #ONE
-	 * @see #EVENS
-	 */
 
+	// ProbabilityZero is a convenient constant representing a probability of
+	// zero, that is an event that will never happen (it is an impossibility).
 	ProbabilityZero = 0
 )
 
+// NewProbability creates a new Probability value.
 func NewProbability(probability float64) (Probability, error) {
 	if probability < 0 || probability > 1 {
 		return ProbabilityZero, fmt.Errorf("Probability must be in the range 0..1 inclusive, got%v", probability)
@@ -46,21 +33,17 @@ func NewProbability(probability float64) (Probability, error) {
 	return Probability(probability), nil
 }
 
-/**
- * Generates an event according the probability value {@literal p}.
- * @param rng A source of randomness for generating events.
- * @return True with a probability of {@literal p}, false with a probability of
- * {@literal 1 - p}.
- */
+// NewEvent generates an event according to the probability value.
+//
+// In other words NextEvent returns True with a probability of p, false with a
+// probability of 1 - p.
 func (p Probability) NextEvent(rng *rand.Rand) bool {
 	// Don't bother generating an random value if the result is guaranteed.
 	return p == 1 || rng.Float64() < float64(p)
 }
 
-/**
- * The complement of a probability p is 1 - p.  If p = 0.75, then the complement is 0.25.
- * @return The complement of this probability.
- */
+// The Complement of a probability p is 1 - p. For example if p = 0.75, the
+// complement is 0.25.
 func (p Probability) Complement() Probability {
 	return Probability(1 - p)
 }
