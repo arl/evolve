cd85f9284654e43a46f04df660b407f7b0c35b9f
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-09-13 11:33:22 +0200

    Add ByteArrayCrossover operator

---
+++ operators/bytearray_crossover.go
@@ -0,0 +1,43 @@
+package operators
+
+import (
+	"math/rand"
+
+	"github.com/aurelien-rainone/evolve/base"
+)
+
+// NewByteArrayCrossover creates a crossover operator for array of primitive
+// ints.
+func NewByteArrayCrossover(options ...func(*AbstractCrossover) error) (*AbstractCrossover, error) {
+	return NewAbstractCrossover(byteArrayMater{}, options...)
+}
+
+type byteArrayMater struct{}
+
+func (m byteArrayMater) Mate(parent1, parent2 base.Candidate,
+	numberOfCrossoverPoints int64,
+	rng *rand.Rand) []base.Candidate {
+
+	p1, p2 := parent1.([]byte), parent2.([]byte)
+
+	if len(p1) != len(p2) {
+		panic("Cannot perform cross-over with different length parents.")
+	}
+	offspring1 := make([]byte, len(p1))
+	copy(offspring1, p1)
+	offspring2 := make([]byte, len(p1))
+	copy(offspring2, p2)
+
+	// Apply as many cross-overs as required.
+	for i := int64(0); i < numberOfCrossoverPoints; i++ {
+		// Cross-over index is always greater than zero and less than
+		// the length of the parent so that we always pick a point that
+		// will result in a meaningful cross-over.
+		crossoverIndex := (1 + rng.Intn(len(p1)-1))
+		for j := 0; j < crossoverIndex; j++ {
+			// swap elements j of both offsprings
+			offspring1[j], offspring2[j] = offspring2[j], offspring1[j]
+		}
+	}
+	return append([]base.Candidate{}, offspring1, offspring2)
+}
---
+++ operators/bytearray_crossover_test.go
@@ -0,0 +1,64 @@
+package operators
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/aurelien-rainone/evolve/base"
+	"github.com/aurelien-rainone/evolve/number"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestByteArrayCrossover(t *testing.T) {
+	rng := rand.New(rand.NewSource(99))
+
+	crossover, err := NewByteArrayCrossover()
+	assert.Nil(t, err)
+
+	population := make([]base.Candidate, 4)
+	population[0] = []byte{1, 2, 3, 4, 5}
+	population[1] = []byte{6, 7, 8, 9, 10}
+	population[2] = []byte{11, 12, 13, 14, 15}
+	population[3] = []byte{16, 17, 18, 19, 20}
+
+	for i := 0; i < 20; i++ {
+		values := make(map[byte]struct{}, 20) // used as a set of runes
+		population = crossover.Apply(population, rng)
+		assert.Len(t, population, 4, "Population size changed after cross-over.")
+		for _, individual := range population {
+			s := individual.([]byte)
+			assert.Lenf(t, s, 5, "Invalid candidate length: %v", len(s))
+			for _, value := range s {
+				values[value] = struct{}{}
+			}
+		}
+		// All of the individual elements should still be present, just jumbled up
+		// between individuals.
+		assert.Len(t, values, 20, "Information lost during cross-over.")
+	}
+}
+
+// The ByteArrayCrossover operator is only defined to work on populations
+// containing arrays of equal lengths. Any attempt to apply the operation to
+// populations that contain different length arrays should panic. Not panicking
+// should be considered a bug since it could lead to hard to trace bugs
+// elsewhere.
+func TestByteArrayCrossoverWithDifferentLengthParents(t *testing.T) {
+	rng := rand.New(rand.NewSource(99))
+
+	crossover, err := NewByteArrayCrossover(
+		WithConstantCrossoverPoints(1),
+		WithConstantCrossoverProbability(number.ProbabilityOne),
+	)
+	assert.Nil(t, err)
+
+	population := make([]base.Candidate, 2)
+	population[0] = []byte{1, 2, 3, 4, 5}
+	population[1] = []byte{2, 4, 8, 10, 12, 14, 16}
+
+	// This should panic since the parents are different lengths.
+	// TODO: why panicking and not returning an error?
+	assert.Panics(t, func() {
+		crossover.Apply(population, rng)
+	})
+}
