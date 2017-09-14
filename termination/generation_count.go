077a17722fc56d4b1d090d6c01ae72cb663a1dc9
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-09-14 15:38:05 +0200

    Add GenerationCount termination condition

---
+++ termination/generation_count.go
@@ -0,0 +1,27 @@
+package termination
+
+import "github.com/aurelien-rainone/evolve/base"
+
+// GenerationCount terminates evolution after a set number of generations have
+// passed.
+type GenerationCount struct {
+	generationCount int
+}
+
+// NewGenerationCount creates a GenerationCoun termination condition.
+func NewGenerationCount(generationCount int) GenerationCount {
+	if generationCount <= 0 {
+		panic("Generation count must be positive")
+	}
+	return GenerationCount{generationCount: generationCount}
+}
+
+// ShouldTerminate reports whether or not evolution should finish at the
+// current point.
+//
+// populationData is the information about the current state of evolution.
+// This may be used to determine whether evolution should continue or not.
+
+func (tc GenerationCount) ShouldTerminate(populationData base.PopulationData) bool {
+	return populationData.GenerationNumber()+1 >= tc.generationCount
+}
