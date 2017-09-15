9c71e06535ecb979a539091be77f6758ac291996
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-09-15 19:16:58 +0200

    Add comparison method to EvaluatedCandidate

--- base/evaluated_candidate.go
+++ base/evaluated_candidate.go
@@ -29,3 +29,30 @@ func (ec *EvaluatedCandidate) Candidate() Candidate {
 func (ec *EvaluatedCandidate) Fitness() float64 {
 	return ec.fitness
 }
+
+// Equals returns true If this object is logically equivalent to {code o}.
+func (ec *EvaluatedCandidate) Equals(o *EvaluatedCandidate) bool {
+	if ec == o {
+		return true
+	} else if o == nil {
+		return false
+	}
+	return ec.Fitness() == o.Fitness()
+}
+
+// CompareTo compares this candidate's fitness score with that of the specified
+// candidate.
+//
+// Returns -1, 0 or 1 if this candidate's score is less than, equal to, or
+// greater than that of the specified candidate. The comparison applies to the
+// raw numerical score and does not consider whether that score is a natural
+// fitness score or not.
+func (ec *EvaluatedCandidate) CompareTo(o *EvaluatedCandidate) int {
+	switch {
+	case ec.Fitness() < o.Fitness():
+		return -1
+	case ec.Fitness() > o.Fitness():
+		return 1
+	}
+	return 0
+}
