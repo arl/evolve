78c61828be7440ddf884c9c82a1cb1f1217a55a4
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-09-29 00:52:33 +0200

    Add Float64Generator and variants

--- number/generator.go
+++ number/generator.go
@@ -1,5 +1,11 @@
 package number
 
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
 // IntegerGenerator is the interface implemented by objects providing sequence
 // of integers.
 type IntegerGenerator interface {
@@ -23,6 +29,55 @@ func NewConstantIntegerGenerator(constant int64) IntegerGenerator {
 // NextValue returns the next value from the generator.
 func (g ConstantIntegerGenerator) NextValue() int64 {
 	return g.constant
+}
+
+// Float64Generator is the interface implemented by objects providing sequence
+// of float64
+type Float64Generator interface {
+
+	// NextValue returns the next value from the generator.
+	NextValue() float64
+}
+
+// ConstantFloat64Generator is an integer generator that always returns the same
+// value.
+type ConstantFloat64Generator struct {
+	constant float64
+}
+
+// NewConstantFloat64Generator returns an Float64Generator that always returns
+// the provided constant.
+func NewConstantFloat64Generator(constant float64) Float64Generator {
+	return ConstantFloat64Generator{constant: constant}
+}
+
+// NextValue returns the next value from the generator.
+func (g ConstantFloat64Generator) NextValue() float64 {
+	return g.constant
+}
+
+// BoundedFloat64Generator produces float64 bounded in a given range
+type BoundedFloat64Generator struct {
+	min, max float64    // both min and max are exclusive
+	rng      *rand.Rand // PRNG used to generate the values
+}
+
+// NewBoundedFloat64Generator returns a BoundedFloat64Generator that uses a
+// default uniform PRNG.
+func NewBoundedFloat64Generator(min, max float64) *BoundedFloat64Generator {
+	g := &BoundedFloat64Generator{min: min, max: max}
+	g.SetRNG(rand.New(rand.NewSource(int64(time.Now().UnixNano()))))
+	return g
+}
+
+// SetRNG sets the pseudo random number generator used to produce the values.
+func (g *BoundedFloat64Generator) SetRNG(rng *rand.Rand) {
+	g.rng = rng
+}
+
+// NextValue returns the next value from the generator.
+func (g *BoundedFloat64Generator) NextValue() float64 {
+	return g.rng.Float64() + math.SmallestNonzeroFloat64
 }
 
 // ProbabilityGenerator is the interface implemented by objects providing sequence
