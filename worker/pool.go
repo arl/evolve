459979b8525479b5b6dd51a16a7a5e938d86fe60
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-10-03 12:07:53 +0200

    Add worker subpkg (Pool & Worker interface)

---
+++ worker/pool.go
@@ -0,0 +1,72 @@
+package pool
+
+import (
+	"sync"
+)
+
+// A Worker represents a self-contained worker and its load of work, performed
+// when Work is called. It should be self-contained as Worker objects are meant
+// to execute their work concurrently with other workers.
+type Worker interface {
+
+	// Work performs the work and returns the result once done.
+	Work() interface{}
+}
+
+// A Pool manages the concurrent execution of multiple Workers.
+type Pool struct {
+	maxConcurrency int
+}
+
+// NewPool creates a Pool that may use concurrent goroutines to perform the work
+// of some Workers objects.
+//
+// maxConcurrency limits the number of concurrent goroutines, so that at any
+// moment, there will be at most maxConcurrency goroutines. If maxConcurrency is
+// set to 1 the workers will run synchronously.
+func NewPool(maxConcurrency int) *Pool {
+	return &Pool{
+		maxConcurrency: maxConcurrency,
+	}
+}
+
+// Submit indicates to the pool the workers the workers to be run concurrently.
+// The workers will be started in any order and may run concurrently, depending
+// on the maxConcurrency defined at pool creation.
+//
+// The returned slice will contain the results, indexed as the workers.
+func (w *Pool) Submit(workers []Worker) (results []interface{}, err error) {
+	var (
+		throttle = make(chan int, w.maxConcurrency) // used as a limiter
+		wg       sync.WaitGroup
+	)
+
+	// create the slice to store the results
+	results = make([]interface{}, len(workers))
+	for i := range workers {
+
+		// increment the rate limiter
+		throttle <- 1
+
+		// increment the work group
+		wg.Add(1)
+
+		go func(idx int) {
+
+			// ensure the waitgroup is decremented, as well as the throttle
+			// channel, once the worker has eneded
+			defer func() {
+				wg.Done()
+				<-throttle
+			}()
+
+			// perform the work
+			results[idx] = workers[idx].Work()
+		}(i)
+	}
+
+	// wait for all the tasks to be completed
+	wg.Wait()
+	close(throttle)
+	return results, nil
+}
---
+++ worker/pool_test.go
@@ -0,0 +1,105 @@
+package pool
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func randomSeed() int64 {
+	return int64(time.Now().UnixNano())
+}
+
+const sleepTimeMs = 500
+
+type waiter struct{}
+
+func (w waiter) Work() interface{} {
+	time.Sleep(sleepTimeMs * time.Millisecond)
+	return struct{}{}
+}
+
+func TestConcurrentWorkerRatio(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping TestConcurrentWorkerRatio in short mode")
+	}
+
+	const epsilon = 0.05 // accepted ratio error (5%)
+
+	var tests = []struct {
+		count       int     // number of works/workers
+		concurrency int     // max concurrent workers
+		ratio       float64 // expected ratio
+	}{
+		{20, 10, 2},
+		{20, 4, 5},
+		{20, 2, 10},
+		{20, 1, 20},
+	}
+
+	for _, tt := range tests {
+
+		workers := make([]Worker, tt.count)
+		for i := 0; i < tt.count; i++ {
+			workers[i] = waiter{}
+		}
+
+		pool := NewPool(tt.concurrency)
+
+		start := time.Now()
+		_, err := pool.Submit(workers)
+		assert.NoError(t, err)
+
+		// returns the ratio between the actual elapsed time and the time spent
+		// in one worker. The more concurrent the workers do their job, the less
+		// the ratio is.
+		ratio := float64(time.Since(start)/time.Millisecond) / float64(sleepTimeMs)
+		//t.Logf("ratio for %v|%v => %v, expected: %v\n", tt.count, tt.concurrency, ratio, tt.ratio)
+		assert.InEpsilon(t, ratio, tt.ratio, tt.ratio*epsilon)
+	}
+}
+
+type dummyWorker struct {
+	idx int
+}
+
+func (w dummyWorker) Work() interface{} {
+	// sleep for a random number of millliseconds ([50, 300])
+	ms := 50 + rand.Intn(250)
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+	return w.idx
+}
+
+func TestConcurrentWorkerResults(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping TestConcurrentWorkerResults in short mode")
+	}
+
+	var tests = []struct {
+		concurrency int // max concurrent workers
+	}{
+		{10},
+		{4},
+		{2},
+		{1},
+	}
+
+	for _, tt := range tests {
+
+		workers := make([]Worker, 20)
+		for i := range workers {
+			workers[i] = dummyWorker{idx: i}
+		}
+		pool := NewPool(tt.concurrency)
+
+		results, err := pool.Submit(workers)
+		assert.NoError(t, err)
+
+		// check the results slice is indexed as the workers
+		for i, result := range results {
+			assert.Equal(t, result, workers[i].(dummyWorker).idx)
+		}
+	}
+}
