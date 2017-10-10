1c72d5aafc9b5ba518bf2b046b2daa956c1066b1
Aurélien Rainone <aurelien.rainone@gmail.com>
2017-10-10 18:20:19 +0200

    Add BitStringCrossover + tests

---
+++ operators/bitstring_crossover.go
@@ -0,0 +1,44 @@
+package operators
+
+import (
+	"math/rand"
+
+	"github.com/aurelien-rainone/evolve/bitstring"
+	"github.com/aurelien-rainone/evolve/framework"
+)
+
+// NewBitStringCrossover creates a crossover operator with a configurable number
+// of points (fixed or random) for bit strings candidates.
+func NewBitStringCrossover(options ...AbstractCrossoverOption) (*AbstractCrossover, error) {
+	return NewAbstractCrossover(bitStringMater{}, options...)
+}
+
+type bitStringMater struct{}
+
+// Mate performs cross-over on a pair of parents to generate a pair of
+// offspring.
+//
+// parent1 and parent2 are the two individuals that provides the source
+// material for generating offspring.
+func (m bitStringMater) Mate(parent1, parent2 framework.Candidate,
+	numberOfCrossoverPoints int64,
+	rng *rand.Rand) []framework.Candidate {
+
+	p1, p2 := parent1.(*bitstring.BitString), parent2.(*bitstring.BitString)
+
+	if p1.Len() != p2.Len() {
+		panic("Cannot perform cross-over with different length parents")
+	}
+	offspring1 := p1.Clone()
+	offspring2 := p2.Clone()
+
+	// Apply as many cross-overs as required.
+	for i := int64(0); i < numberOfCrossoverPoints; i++ {
+		// Cross-over index is always greater than zero and less than the
+		// length of the parent so that we always pick a point that will
+		// result in a meaningful cross-over.
+		crossoverIndex := (1 + rng.Intn(p1.Len()-1))
+		offspring1.SwapSubstring(offspring2, 0, crossoverIndex)
+	}
+	return []framework.Candidate{offspring1, offspring2}
+}
