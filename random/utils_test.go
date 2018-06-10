69448dbb0f340423df477f151844c173aeeeef1a
Aurélien Rainone <aurelien.rainone@gmail.com>
2018-06-10 22:07:43 +0200

    random: renamings, cosmetics

--- random/utils_test.go
+++ random/utils_test.go
@@ -13,30 +13,30 @@ import (
 // distributed, this method will return a poor estimate for the
 // value of pi.
 // - rng is the RNG to test.
-// - iterations is the number of random points to generate for use in the
+// - niter is the number of random points to generate for use in the
 // calculation. This value needs to be sufficiently large in order to
 // produce a reasonably accurate result (assuming the RNG is uniform).
 // Less than 10,000 is not particularly useful.  100,000 should be sufficient.
 //
 // Returns an approximation of pi generated using the provided RNG.
-func calculateMonteCarloValueForPi(rng *rand.Rand, iterations int) float64 {
+func monteCarloValueForPi(rng *rand.Rand, niter int) float64 {
 	// Assumes a quadrant of a circle of radius 1, bounded by a box with
 	// sides of length 1.  The area of the square is therefore 1 square unit
 	// and the area of the quadrant is (pi * r^2) / 4.
-	var totalInsideQuadrant int
+	var tot int
 	// Generate the specified number of random points and count how many fall
 	// within the quadrant and how many do not. We expect the number of points
 	// in the quadrant (expressed as a fraction of the total number of points)
 	// to be pi/4. Therefore pi = 4 * ratio.
-	for i := 0; i < iterations; i++ {
+	for i := 0; i < niter; i++ {
 		x := rng.Float64()
 		y := rng.Float64()
 		if isInQuadrant(x, y) {
-			totalInsideQuadrant++
+			tot++
 		}
 	}
 	// From these figures we can deduce an approximate value for Pi.
-	return 4 * float64(totalInsideQuadrant) / float64(iterations)
+	return 4 * float64(tot) / float64(niter)
 }
 
 // Uses Pythagoras' theorem to determine whether the specified coordinates fall
@@ -53,16 +53,16 @@ func isInQuadrant(x, y float64) bool {
 // Generates a sequence of values from a given random number generator and
 // then calculates the standard deviation of the sample.
 // - rng is the RNG to use.
-// - param maxValue is the maximum value for generated integers (values will
-// be in the range [0, maxValue)).
-// - iterations is the number of values to generate and use in the standard
-// deviation calculation.
+// - max is the maximum value for generated integers (values will be in the
+// range [0, max)).
+// - niter is the number of values to generate and use in the standard deviation
+// calculation.
 //
 // Returns the standard deviation of the generated sample.
-func calculateSampleStandardDeviation(rng *rand.Rand, maxValue int64, iterations int) float64 {
-	dataset := api.NewDataset(iterations)
-	for i := 0; i < iterations; i++ {
-		dataset.AddValue(float64(rng.Int63n(maxValue)))
+func calculareSampleStdDev(rng *rand.Rand, max int64, niter int) float64 {
+	dataset := api.NewDataset(niter)
+	for i := 0; i < niter; i++ {
+		dataset.AddValue(float64(rng.Int63n(max)))
 	}
 	return dataset.SampleStandardDeviation()
 }
