e1318bc099ea17fc226bee48b91ae3a7ff49669e
Aurélien Rainone <aurelien.rainone@gmail.com>
2018-06-16 16:47:13 +0200

    Use goroutines for evaluation, remove worker pkg

--- evaluation.go
+++ evaluation.go
@@ -1,11 +1,6 @@
 package evolve
 
-import (
-	"fmt"
-	"runtime"
-
-	"github.com/arl/evolve/worker"
-)
+import "sync"
 
 // EvaluatePopulation takes a population, assigns a fitness score to each member
 // and returns the members with their scores attached, sorted in descending
@@ -17,12 +12,11 @@ import (
 // Returns the evaluated population (a slice of candidates with attached fitness
 // scores).
 func EvaluatePopulation(pop []interface{}, e Evaluator, concurrent bool) Population {
-
-	// Do fitness evaluations
-	evpop := make(Population, len(pop))
+	var evpop Population
 
 	if !concurrent {
 
+		evpop = make(Population, len(pop))
 		for i, candidate := range pop {
 			evpop[i] = &Individual{
 				Candidate: candidate,
@@ -32,52 +26,26 @@ func EvaluatePopulation(pop []interface{}, e Evaluator, concurrent bool) Populat
 
 	} else {
 
-		// Create a worker pool that will divides the required number of fitness
-		// evaluations equally among the available goroutines and coordinate
-		// them so that we do not proceed until all of them have finished
-		// processing.
-		wp := worker.NewPool(runtime.NumCPU())
-		workers := make([]worker.Worker, len(pop))
+		evpop = make(Population, len(pop))
+
+		var w sync.WaitGroup
+		w.Add(len(pop))
 
 		for i := range pop {
-			workers[i] = &fitnessWorker{
-				idx:       i,
-				pop:       pop,
-				evaluator: e,
-			}
+			go func(i int) {
+				ind := &Individual{
+					Candidate: pop[i],
+					Fitness:   e.Fitness(pop[i], pop),
+				}
+				evpop[i] = ind
+				w.Done()
+			}(i)
 		}
 
-		results, err := wp.Submit(workers)
-		if err != nil {
-			panic(fmt.Sprintf("Error while submitting workers to the pool: %v", err))
-		}
+		w.Wait()
 
-		for i, result := range results {
-			evpop[i] = result.(*Individual)
-		}
 		// TODO: handle goroutine termination
-		/*
-		   catch (InterruptedException ex)
-		   {
-		       // Restore the interrupted status, allows methods further up the call-stack
-		       // to abort processing if appropriate.
-		       Thread.currentThread().interrupt();
-		   }
-		*/
 	}
 
 	return evpop
-}
-
-type fitnessWorker struct {
-	idx       int           // index of candidate to evaluate
-	pop       []interface{} // full population
-	evaluator Evaluator
-}
-
-func (w *fitnessWorker) Work() (interface{}, error) {
-	return &Individual{
-		Candidate: w.pop[w.idx],
-		Fitness:   w.evaluator.Fitness(w.pop[w.idx], w.pop),
-	}, nil
 }
