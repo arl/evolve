08fc4bd9fb0b1fe0c6ea204dfec2f8d51cc75365
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-03-28 11:58:31 +0200

    Cosmetics

--- example/doc.go
+++ example/doc.go
@@ -1,2 +1,3 @@
-// Package example contains various evolutionary algorithm implementations.
+// Package example demonstrates multiples examples applications using the evolve
+// library to solve problems with evolutionnary algorithms.
 package example
