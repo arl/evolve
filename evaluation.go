45f14f438df027bbf50b15071ed9a437fd23ea4c
Aurélien Rainone <aurelien.rainone@gmail.com>
2021-03-28 11:54:27 +0200

    Cosmetics

--- evaluation.go
+++ evaluation.go
@@ -6,15 +6,15 @@ import "sync"
 //
 // Every individual is assigned a fitness score with the provided evaluator,
 // after that individuals is inserted into a population. The population is then
-// sorted, either in descending order of fitness for natural scores, or ascending
-// for non natural scores.
+// sorted, either in descending order of fitness for natural scores, or
+// ascending for non-natural scores.
 //
-// Returns the evaluated population (a slice of individuals, each of which associated with its fitness).
+// Returns the evaluated population (a slice of individuals, each of which
+// associated with its fitness).
 func EvaluatePopulation(pop []interface{}, e Evaluator, concurrent bool) Population {
 	var evpop Population
 
 	if !concurrent {
-
 		evpop = make(Population, len(pop))
 		for i, candidate := range pop {
 			evpop[i] = &Individual{
@@ -22,9 +22,7 @@ func EvaluatePopulation(pop []interface{}, e Evaluator, concurrent bool) Populat
 				Fitness:   e.Fitness(candidate, pop),
 			}
 		}
-
 	} else {
-
 		evpop = make(Population, len(pop))
 
 		var w sync.WaitGroup
