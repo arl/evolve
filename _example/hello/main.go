7972b158331e0c9100e2f1abd7c5a78553a300d9
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-01-04 11:08:37 +0100

    _example/hello: make it work

--- _example/hello/main.go
+++ _example/hello/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
 
@@ -27,26 +28,6 @@ func check(err error) {
 	}
 }
 
-// This evaluator assigns one "fitness point" for every character in the
-// candidate string that doesn't match the corresponding position in the
-// target string.
-type evaluator struct{}
-
-func (evaluator) Fitness(cand interface{}, pop []interface{}) float64 {
-	// count differences between candidate and target strings
-	var nerrors int
-	sc := cand.(string)
-	for i := range sc {
-		if sc[i] != target[i] {
-			nerrors++
-		}
-	}
-	return float64(nerrors)
-}
-
-// Non natural fitness, lower is better
-func (evaluator) IsNatural() bool { return false }
-
 func main() {
 	if len(os.Args) == 2 {
 		target = strings.ToUpper(os.Args[1])
@@ -55,41 +36,59 @@ func main() {
 	// Setup a generator of random strings
 	for _, c := range target {
 		if !strings.ContainsRune(alphabet, c) {
-			log.Fatalf("Target string must be solely made of \"%v\"", alphabet)
+			log.Fatalf("Target string must only be made of runes in %q", alphabet)
 		}
 	}
-	gen, err := generator.NewString(alphabet, len(target))
-	check(err)
+
+	evaluator := evolve.EvaluatorFunc[string](false, func(cand string, _ []string) float64 {
+		// Assigns one "fitness point" for every character in the candidate
+		// string that doesn't match the corresponding position in the
+		// target string.
+		var n int
+		for i := range cand {
+			if cand[i] != target[i] {
+				n++
+			}
+		}
+		return float64(n)
+	})
 
 	// Define our evolutionary operators, a string mutation where each rune has
 	// a probability of mutation of 0.02, plus a default string crossover.
-	mutation := mutation.New(&mutation.String{
+	mutation := mutation.New[string](&mutation.String{
 		Alphabet:    alphabet,
-		Probability: generator.ConstFloat64(0.02),
+		Probability: generator.Const(0.02),
 	})
-	xover := xover.New(xover.StringMater{})
-	xover.Points = generator.Const(1.0)
+	xover := xover.New[string](xover.StringMater{})
+	xover.Points = generator.Const(1)
 	xover.Probability = generator.Const(1.0)
 
 	// Define a composite evolutionary operator, that is a pipeline that applies
 	// to each candidate a string mutation followed by a crossover
-	pipeline := operator.Pipeline{mutation, xover}
+	pipeline := operator.Pipeline[string]{mutation, xover}
 
 	// This evaluator assigns one "fitness point" for every character in the
 	// The epocher is generational evolutionary engine.
-	epocher := engine.Generational{
+	epocher := engine.Generational[string]{
 		Op:   pipeline,
-		Eval: evaluator{},
-		Sel:  selection.RouletteWheel,
+		Eval: evaluator,
+		Sel:  selection.RouletteWheel[string]{},
 	}
 
+	nchars := len(target)
 	// Define the components of our engine
-	eng, err := engine.New(gen, evaluator{}, &epocher)
+	eng, err := engine.New[string](evolve.FactoryFunc[string](func(rng *rand.Rand) string {
+		b := make([]byte, nchars)
+		for i := 0; i < nchars; i++ {
+			b[i] = alphabet[rng.Int31n(int32(nchars))]
+		}
+		return string(b)
+	}), evaluator, &epocher)
 	check(err)
 
 	// Define an observer
 	eng.AddObserver(
-		engine.ObserverFunc(func(stats *evolve.PopulationStats) {
+		engine.ObserverFunc(func(stats *evolve.PopulationStats[string]) {
 			log.Printf("Generation %d: %s (%v)\n",
 				stats.GenNumber,
 				stats.BestCand,
@@ -98,10 +97,14 @@ func main() {
 
 	// Evolution should end when a candidate with a fitness of 0 has been
 	// reached (0 different chars between candidate and target string)
-	cond := condition.TargetFitness{Fitness: 0, Natural: false}
+	cond := engine.EndOn[string](condition.TargetFitness[string]{Fitness: 0, Natural: false})
 
 	// Start evolution engine and print the best result
-	bests, _, err := eng.Evolve(100, engine.Elites(5), engine.EndOn(cond))
+	bests, _, err := eng.Evolve(
+		100,                      // population zize
+		engine.Elites[string](5), // number of elites, if any
+		cond,
+	)
 	check(err)
 	log.Println(bests[0])
 }
