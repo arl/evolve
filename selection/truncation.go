6720a18d3c152c56ddd9e15c143ef5b846674047
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-01-17 21:27:25 +0100

    selection: Truncation uses a generator for SelectionRatio

--- selection/truncation.go
+++ selection/truncation.go
@@ -1,76 +1,30 @@
 package selection
 
 import (
-	"errors"
-	"fmt"
+	"constraints"
 	"math"
 	"math/rand"
 
 	"github.com/arl/evolve"
+	"github.com/arl/evolve/generator"
 )
-
-// ErrInvalidTruncRatio is the error returned when trying to set an invalid
-// selection ratio for truncation selection
-var ErrInvalidTruncRatio = errors.New("truncation selection ratio must be in the (0,1] range")
 
 // Truncation implements the selection of n candidates from a population by
 // simply selecting the n candidates with the highest fitness scores (the rest
 // is discarded). The same candidate is never selected more than once.
-// TODO should use number generators here
 type Truncation[T any] struct {
-	ratio, minratio, maxratio float64
-	varratio                  bool
+	// TODO: document
+	SelectionRatio generator.Float
 }
 
-// NewTruncation creates a TruncationSelection configured with the
-// provided options.
-//
-// If no options are provided the selection ratio will vary uniformly between 0
-// and 1.
-func NewTruncation[T any]() *Truncation[T] {
-	return &Truncation[T]{varratio: true, minratio: 0.5, maxratio: 1.0}
-}
-
-// SetRatio sets a constant selection ratio, that is the proportion of the
-// highest ranked candidates to select from the population.
-//
-// If ratio is not in the (0,1] range SetRatio will return ErrInvalidTruncRatio
-func (ts *Truncation[T]) SetRatio(ratio float64) error {
-	if ratio <= 0.0 || ratio > 1.0 {
-		return ErrInvalidTruncRatio
-	}
-	ts.ratio = ratio
-	ts.varratio = false
-	return nil
-}
-
-// SetRatioRange sets the range of possible truncation selection ratio.
-//
-// The specific ratio will be randomly chosen with the pseudo random number
-// generator argument of Select, by linearly converting from (0.5,1.0) to
-// [min,max).
-//
-// If min and max are not bounded by [0,1] SetRatioRange will return
-// ErrInvalidTruncRatio.
-func (ts *Truncation[T]) SetRatioRange(min, max float64) error {
-	if min > max || min <= 0.0 || max > 1.0 {
-		return ErrInvalidTruncRatio
-	}
-	ts.minratio = min
-	ts.maxratio = max
-	ts.varratio = true
-	return nil
-}
-
-// Select selects the fittest candidates. If the selectionRatio results in
-// fewer selected candidates than required, then these candidates are
-// selected multiple times to make up the shortfall.
+// Select selects the fittest candidates. If the SelectionRatio results in fewer
+// selected candidates than required, then these candidates are selected
+// multiple times to make up the shortfall.
 //
 // pop is the population of evolved and evaluated candidates from which to
-// select.
-// natural indicates whether higher fitness values represent fitter individuals
-// or not.
-// size is the number of candidates to select from the evolved population.
+// select. natural indicates whether higher fitness values represent fitter
+// individuals or not. size is the number of candidates to select from the
+// evolved population.
 //
 // Returns the selected candidates.
 func (ts *Truncation[T]) Select(pop evolve.Population[T], natural bool, size int, rng *rand.Rand) []T {
@@ -78,18 +32,13 @@ func (ts *Truncation[T]) Select(pop evolve.Population[T], natural bool, size int
 
 	// get a random value to decide wether to select the fitter individual
 	// or the weaker one.
-	ratio := ts.ratio
-	if ts.varratio {
-		ratio = ts.minratio + (ts.maxratio-ts.minratio)*rng.Float64()
-	}
-
-	eligible := int(math.Round(ratio * float64(len(pop))))
+	eligible := int(math.Round(ts.SelectionRatio.Next() * float64(len(pop))))
 	if eligible > size {
 		eligible = size
 	}
 
 	for {
-		count := minint(eligible, size-len(sel))
+		count := min(eligible, size-len(sel))
 		for i := 0; i < count; i++ {
 			sel = append(sel, pop[i].Candidate)
 		}
@@ -101,15 +50,10 @@ func (ts *Truncation[T]) Select(pop evolve.Population[T], natural bool, size int
 }
 
 func (ts *Truncation[T]) String() string {
-	s := "Truncation Selection (%v%%)"
-	if ts.varratio {
-		return fmt.Sprintf(s, fmt.Sprintf("%5.2f-%5.2f", 100*ts.minratio, 100*ts.maxratio))
-	}
-	return fmt.Sprintf(s, fmt.Sprintf("%5.2f", 100*ts.ratio))
+	return "Truncation Selection"
 }
 
-// minint returns the minimum of two int values.
-func minint(a, b int) int {
+func min[T constraints.Ordered](a, b T) T {
 	if a < b {
 		return a
 	}
--- selection/truncation_test.go
+++ selection/truncation_test.go
@@ -2,17 +2,16 @@ package selection
 
 import (
 	"fmt"
-	"math"
 	"testing"
 
+	"github.com/arl/evolve/generator"
 	"github.com/stretchr/testify/assert"
 )
 
 // Unit test for truncation selection strategy ensures the 2 best candidates are
 // selected.
 func testTruncationSelection(t *testing.T, tpop testPopulation, natural bool) {
-	ts := NewTruncation[string]()
-	errcheck(t, ts.SetRatio(0.5))
+	ts := &Truncation[string]{SelectionRatio: generator.Const(0.5)}
 	testRandomBasedSelection(t, ts, tpop, natural, 2,
 		func(selected []string) error {
 			if len(selected) != 2 {
@@ -36,43 +35,4 @@ func TestTruncationSelectionNatural(t *testing.T) {
 
 func TestTruncationSelectionNonNatural(t *testing.T) {
 	testTruncationSelection(t, randomBasedPopNonNatural, false)
-}
-
-func TestTruncationSelectionSetRatio(t *testing.T) {
-	tests := []struct {
-		ratio   float64
-		wantErr error
-	}{
-		{ratio: -1, wantErr: ErrInvalidTruncRatio},
-		{ratio: 0, wantErr: ErrInvalidTruncRatio},
-		{ratio: 1.00001, wantErr: ErrInvalidTruncRatio},
-		{ratio: math.SmallestNonzeroFloat64, wantErr: nil},
-		{ratio: 0.5, wantErr: nil},
-		{ratio: 1.0, wantErr: nil},
-	}
-	for _, tt := range tests {
-		if got := NewTruncation[string]().SetRatio(tt.ratio); got != tt.wantErr {
-			t.Errorf("SetRatio(%v), got err = %v, wantErr = %v", tt.ratio, got, tt.wantErr)
-		}
-	}
-}
-
-func TestTruncationSelectionSetRatioRange(t *testing.T) {
-	tests := []struct {
-		min, max float64
-		wantErr  error
-	}{
-		{min: 0, max: 1, wantErr: ErrInvalidTruncRatio},
-		{min: -1, max: 1, wantErr: ErrInvalidTruncRatio},
-		{min: 0, max: 1.00001, wantErr: ErrInvalidTruncRatio},
-		{min: 0.2, max: 0.1, wantErr: ErrInvalidTruncRatio},
-		{min: 0.1, max: 0.2, wantErr: nil},
-		{min: 0.1, max: 1, wantErr: nil},
-		{min: 0.1, max: 1.01, wantErr: ErrInvalidTruncRatio},
-	}
-	for _, tt := range tests {
-		if got := NewTruncation[string]().SetRatioRange(tt.min, tt.max); got != tt.wantErr {
-			t.Errorf("SetRatioRange(%v, %v), got err = %v, wantErr = %v", tt.min, tt.max, got, tt.wantErr)
-		}
-	}
 }
