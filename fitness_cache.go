446e840bf142994877acf3f70fb4136a65d23001
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-01-22 21:20:56 +0100

    Cosmetics

--- fitness_cache.go
+++ fitness_cache.go
@@ -2,27 +2,27 @@ package evolve
 
 import "sync"
 
-// FitnessCache provides caching for any Evaluator implementation. The
-// results of fitness evaluations are stored in a cache so that if the same
-// candidate is evaluated twice, the expense of the fitness calculation can be
-// avoided the second time.
+// FitnessCache provides caching for any Evaluator implementation. The results
+// of fitness evaluations are stored in a cache so that if the same candidate is
+// evaluated twice, the expense of the fitness calculation can be avoided the
+// second time.
 //
-// Caching of fitness values can be a useful optimisation in situations where the
-// fitness evaluation is expensive and there is a possibility that some candidates
-// will survive from generation to generation unmodified.  Programs that use elitism
-// are one example of candidates surviving unmodified.  Another scenario is when the
-// configured evolutionary operator does not always modify every candidate in the
-// population for every generation.
+// Caching of fitness values can be a useful optimisation in situations where
+// the fitness evaluation is expensive and there is a possibility that some
+// candidates will survive from generation to generation unmodified.  Programs
+// that use elitism are one example of candidates surviving unmodified.  Another
+// scenario is when the configured evolutionary operator does not always modify
+// every candidate in the population for every generation.
 //
 // Caching of fitness scores is only valid when fitness evaluations are isolated
 // and repeatable. An isolated fitness evaluation is one where the result
-// depends only upon the candidate being evaluated.  This is not the case when
-// candidates are evaluated against the other members of the population.  So
+// depends only upon the candidate being evaluated. This is not the case when
+// candidates are evaluated against the other members of the population. So
 // unless the fitness evaluator ignores the second parameter to the
 // Evaluator.Fitness method, caching must not be used.
 type FitnessCache[T any] struct {
 
-	// Wrapped is the fitness evaluator for which we want to provide caching
+	// Wrapped is the fitness evaluator for which we want to provide caching.
 	Wrapped Evaluator[T]
 	cache   sync.Map
 }
