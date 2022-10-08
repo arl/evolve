37a2495f44d3e06f933c347e0e70e8a60260a954
Aurélien Rainone <aurelien.rainone@gmail.com>
2022-10-08 13:45:05 +0200

    operator/mutation: use go-cmp for srs tests

--- operator/mutation/srs_test.go
+++ operator/mutation/srs_test.go
@@ -3,38 +3,43 @@ package mutation
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/google/go-cmp/cmp"
 )
 
 func Test_srs(t *testing.T) {
 	tests := []struct {
 		org      []int
 		xp1, xp2 int
-		mut      []int // want
+		want     []int // want
 	}{
 		{
 			org: []int{1, 2, 3, 4, 5, 6, 7},
 			//              |     |
 			xp1: 2, xp2: 4,
-			mut: []int{5, 6, 7, 3, 4, 2, 1},
+			want: []int{5, 6, 7, 3, 4, 2, 1},
 		},
 		{
 			org: []int{1, 2, 3, 4, 5, 6, 7},
 			//           |  |
 			xp1: 1, xp2: 2,
-			mut: []int{3, 4, 5, 6, 7, 2, 1},
+			want: []int{3, 4, 5, 6, 7, 2, 1},
 		},
 		{
 			org: []int{1, 2, 3, 4, 5, 6, 7},
 			//                       |  |
 			xp1: 5, xp2: 6,
-			mut: []int{7, 6, 5, 4, 3, 2, 1},
+			want: []int{7, 6, 5, 4, 3, 2, 1},
 		},
 	}
 
 	for _, tt := range tests {
-		got := make([]int, len(tt.org))
-		srs(tt.org, got, tt.xp1, tt.xp2)
-		assert.Equal(t, tt.mut, got)
+		// Before calling srs, the slice to mutate is already a copy of the original slice.
+		mut := make([]int, len(tt.org))
+		copy(mut, tt.org)
+
+		srs(tt.org, mut, tt.xp1, tt.xp2)
+		if !cmp.Equal(tt.want, mut) {
+			t.Errorf("srs(%+v, [res], %d, %d) = %+v, want %+v", tt.org, tt.xp1, tt.xp2, mut, tt.want)
+		}
 	}
 }
