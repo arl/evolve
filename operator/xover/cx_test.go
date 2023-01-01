cf7e6e1695d7cf80c6037e5b18db6309d8f52b2f
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-01-01 19:27:37 +0100

    operator/xover: deflake cx tests

--- operator/xover/cx_test.go
+++ operator/xover/cx_test.go
@@ -5,50 +5,54 @@ import (
 	"testing"
 
 	"github.com/arl/evolve/generator"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCX(t *testing.T) {
 	rng := rand.New(rand.NewSource(99))
-
 	xover := New[[]int](CX[int]{})
 	xover.Probability = generator.Const(1.0)
 	xover.Points = generator.Const(1) // unused
 
 	tests := []struct {
 		name       string
-		p1, p2     []int
-		off1, off2 []int
+		p0, p1     []int
+		off0, off1 []int
 	}{
 		{
 			name: "4-elem cycle",
-			p1:   []int{1, 2, 3, 4, 5, 6, 7},
-			p2:   []int{7, 5, 1, 3, 2, 6, 4},
-			off1: []int{1, 5, 3, 4, 2, 6, 7},
-			off2: []int{7, 2, 1, 3, 5, 6, 4},
+			p0:   []int{1, 2, 3, 4, 5, 6, 7},
+			p1:   []int{7, 5, 1, 3, 2, 6, 4},
+			off0: []int{1, 5, 3, 4, 2, 6, 7},
+			off1: []int{7, 2, 1, 3, 5, 6, 4},
 		},
 		{
 			name: "1-elem cycle",
-			p1:   []int{1, 2, 3},
-			p2:   []int{1, 3, 2},
-			off1: []int{1, 3, 2},
-			off2: []int{1, 2, 3},
+			p0:   []int{1, 2, 3},
+			p1:   []int{1, 3, 2},
+			off0: []int{1, 3, 2},
+			off1: []int{1, 2, 3},
 		},
 		{
 			name: "single cycle",
-			p1:   []int{1, 2, 3},
-			p2:   []int{2, 3, 1},
-			off1: []int{1, 2, 3},
-			off2: []int{2, 3, 1},
+			p0:   []int{1, 2, 3},
+			p1:   []int{2, 3, 1},
+			off0: []int{1, 2, 3},
+			off1: []int{2, 3, 1},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			pop := [][]int{tt.p1, tt.p2}
+			pop := [][]int{tt.p0, tt.p1}
 			got := xover.Apply(pop, rng)
-			assert.Equal(t, tt.off1, got[0], "unexpected offspring1")
-			assert.Equal(t, tt.off2, got[1], "unexpected offspring2")
+			off0, off1 := got[0], got[1]
+
+			if !(cmp.Equal(tt.off0, off0) && cmp.Equal(tt.off1, off1)) &&
+				!(cmp.Equal(tt.off0, off1) && cmp.Equal(tt.off1, off0)) {
+				t.Errorf("unexpected offsprings\n%+v\n%+v\n\nwant\n\n%+v\n%+v\n", off0, off1, tt.off0, tt.off1)
+			}
 		})
 	}
 }
